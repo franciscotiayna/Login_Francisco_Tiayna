e9e3489a1c1915b30ea6043de5ec4578f406da5f
franciscotiayna <aetbred@gmail.com>
2018-08-30 14:57:20 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tarrito/login_francisco_tiayna/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tarrito.login_francisco_tiayna;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
